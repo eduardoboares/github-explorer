0447cdb2075a9998973bfed5cae3ad943b4c0e77
eduardopereiraboares <eduardop.boares@gmail.com>
2021-07-15 22:01:45 -0300

    Dashboard styling implemented

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Form, Repositories, Title } from './styles';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no GitHub</Title>;
+const Dashboard: React.FC = () => (
+    <>
+        <img src={logoImg} alt="GitHub Explorer" />
+        <Title>Explore repositórios no GitHub</Title>
+
+        <Form action="">
+            <input placeholder="Digite o nome do repositório" />
+            <button type="submit">Pesquisar</button>
+        </Form>
+
+        <Repositories>
+            <a href="teste">
+                <img
+                    src="https://avatars.githubusercontent.com/u/50203486?v=4"
+                    alt="Eduardo Pereira Boares"
+                />
+                <div>
+                    <strong>Rocketseat/unform</strong>
+                    <p>Performance-focused API for React forms 🚀</p>
+                </div>
+
+                <FiChevronRight size={20} />
+            </a>
+        </Repositories>
+    </>
+);
 
 export default Dashboard;
