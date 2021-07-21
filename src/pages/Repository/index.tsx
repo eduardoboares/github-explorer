80cea743ee855895a6a97c2cb3b1480503d1c7f9
eduardopereiraboares <eduardop.boares@gmail.com>
2021-07-21 01:03:37 -0300

    Route Navigation implemented

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router';
 
-const Repository: React.FC = () => <h1>Repository</h1>;
+interface RepositoryParams {
+    repository: string;
+}
+
+const Repository: React.FC = () => {
+    const { params } = useRouteMatch<RepositoryParams>();
+
+    return <h1>Repository: {params.repository}</h1>;
+};
 
 export default Repository;
